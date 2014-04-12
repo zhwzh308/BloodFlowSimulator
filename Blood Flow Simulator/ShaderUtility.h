344693a768f779d0253f435be0c031477262c71b
Wenzhong Zhang <wenzhong.zhang@yahoo.com>
2014-04-11 20:44:32 -0400

    GLES setup, preview view config.

---
+++ Blood Flow Simulator/ShaderUtility.h
@@ -0,0 +1,35 @@
+//
+//  ShaderUtility.h
+//  Blood Flow Simulator
+//
+//  Created by Wenzhong Zhang on 2014-04-11.
+//  Copyright (c) 2014 Wenzhong Zhang. All rights reserved.
+//
+
+#ifndef Blood_Flow_Simulator_ShaderUtility_h
+#define Blood_Flow_Simulator_ShaderUtility_h
+
+#include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
+
+// shader compiler
+GLint glueCompileShader(GLenum target,
+                        GLsizei count,
+                        const GLchar **sources,
+                        GLuint *shader);
+
+Glint glueLinkProgram(GLuint program);
+GLint glueValidateProgram(GLuint program);
+GLint glueGetUniformLocation(GLuint program, const GLchar *name);
+
+GLint glueCreateProgram(const GLchar *vertSource,
+                        const GLchar *fragSource,
+                        GLsizei attribNameCt,
+                        const GLchar **attribNames,
+                        const GLint *attribLocations,
+                        GLsizei uniformNameCt,
+                        const GLchar **uniformNames,
+                        GLint *uniformLocations,
+                        GLuint *program);
+
+#endif
--- Blood Flow Simulator/simulatorPreviewView.h
+++ Blood Flow Simulator/simulatorPreviewView.h
@@ -7,7 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/EAGLDrawable.h>
+#import <OpenGLES/ES2/glext.h>
+#import <CoreVideo/CVOpenGLESTextureCache.h>
 
 @interface simulatorPreviewView : UIView
+{
+    int renderBufferWidth, renderBufferHeight;
+    // OpenGLES texture cache
+    CVOpenGLESTextureCacheRef videoTextureCache;
+    // In a EAGLContext
+    EAGLContext *oglContext;
+    
+    // All the GL parameters
+    GLuint frameBufferHandle, colorBufferHandle, passThroughProgram;
+}
+
+- (void) displayPixelBuffer: (CVImageBufferRef)pixelBuffer;
 
 @end
